42bee64f40dae97e4fb74a78505476cf87d2c3d6
Haitao Pan <manbuzhe2009@qq.com>
2025-01-08 05:32:46 +0000

    update

--- Solutions/Kafka-Alert-Demo/cn-k3s-all-in-one.sh
+++ Solutions/Kafka-Alert-Demo/cn-k3s-all-in-one.sh
@@ -1,6 +1,12 @@
 sudo mkdir -pv /opt/rancher/k3s
-curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh | INSTALL_K3S_MIRROR=cn INSTALL_K3S_SKIP_SELINUX_RPM=true sh -s - \
---system-default-registry "registry.cn-hangzhou.aliyuncs.com" --data-dir=/opt/rancher/k3s --kube-apiserver-arg service-node-port-range=0-50000
+curl -sfL https://rancher-mirror.rancher.cn/k3s/k3s-install.sh \
+	| INSTALL_K3S_MIRROR=cn             \
+	INSTALL_K3S_SKIP_SELINUX_RPM=true   \
+	sh -s -                             \
+	--data-dir=/opt/rancher/k3s         \
+	--kube-apiserver-arg service-node-port-range=0-50000          \
+	--system-default-registry "registry.cn-hangzhou.aliyuncs.com" \
+	--disable=traefik,servicelb
 #curl -sfL https://get.k3s.io | sh -s - --disable=traefik,servicelb                                   \
 #        --data-dir=/opt/rancher/k3s                                                                  \
 #        --kube-apiserver-arg service-node-port-range=0-50000
