dfdf6d0b9a64bd8a0087c74e5c4ac77667ab5aa4
Haitao Pan <manbuzhe2009@qq.com>
2025-05-23 16:50:25 +0800

    chore: remove scripts,playbooks directory for migration to svc-design/gitops

--- playbooks/roles/vhosts/vault/readme.md
+++
@@ -1,4 +0,0 @@
-# Init Vault Server
-
-kubectl exec -t -i vault-server-0 -n vault -- sh 
-vault operator init -key-shares=5 -key-threshold=3
--- scripts/global-monitor/setup-kubesphere-core.sh
+++
@@ -1,1 +0,0 @@
-helm upgrade --install -n kubesphere-system --create-namespace ks-core https://charts.kubesphere.io/main/ks-core-1.1.2.tgz --debug --wait
