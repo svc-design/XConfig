693bff1806c880b8c583c36dece234e3ff7de154
shenlan <manbuzhe2009@qq.com>
2025-06-04 16:12:43 +0800

    agent: support template task

--- CraftWeaveAgent/src/models.rs
+++ CraftWeaveAgent/src/models.rs
@@ -11,5 +11,12 @@ pub struct Task {
     pub name: String,
     pub shell: Option<String>,
     pub script: Option<String>,
+    pub template: Option<Template>,
+}
+
+#[derive(Debug, Deserialize)]
+pub struct Template {
+    pub src: String,
+    pub dest: String,
 }
 
